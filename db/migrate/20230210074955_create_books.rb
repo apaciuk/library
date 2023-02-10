e9260e688118ac308affeb68f019f18b5657a95e
Paul Anthony McGowan <xhostcom@aol.com>
2023-02-10 07:53:42 +0000

    Books scaffold

--- db/migrate/20230210074955_create_books.rb
+++ db/migrate/20230210074955_create_books.rb
@@ -1,11 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books, id: :uuid do |t|
-      t.string :title
-      t.string :isbn
-      t.string :isbn13
-      t.string :language_code
-      t.integer :num_pages
+      t.string :title, null: false, index: true, default: ""
+      t.string :isbn, null: false, unique: true, index: true, default: ""
+      t.string :isbn13, null: false, unique: true, index: true, default: ""
+      t.string :language_code, null: false, index: true, default: "eng"
+      t.integer :num_pages, null: false, default: 0
 
       t.timestamps
     end
